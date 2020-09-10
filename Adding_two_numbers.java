029329816105184081e0b5371b6013cc2abb9681
kingofbbsr <chandanofficial.cn@gmail.com>
2020-09-10 21:25:54 +0530

    adding two numbers

---
+++ Adding_two_numbers.java
@@ -0,0 +1,13 @@
+
+public class Adding_two_numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=34;
+		int c= a + b;
+		System.out.println(c);
+
+	}
+
+}
