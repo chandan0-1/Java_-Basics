86dab01a1fca90f3b17c35d532ad9ae1d1ede93c
kingofbbsr <chandanofficial.cn@gmail.com>
2020-09-26 22:27:29 +0530

    added Longest Non-Palindromic String

---
+++ Bonus Problem in String/FirstPalindrome.py
@@ -0,0 +1,34 @@
+## Read input as specified in the question.
+## Print output as specified in the question.
+s=input()
+arr=[0]*256
+# for i in s:
+#     arr[ord(i)]+=1
+s=sorted(s)
+v=0
+ans=""
+for i in s:
+    if s.count(i)%2==1:
+        v+=1
+if v>1:
+    print("")
+else:
+    
+    tmp=""
+    if len(s)%2==1:
+        for i in s:
+            if s.count(i)==1:
+                s.remove(i)
+                tmp=i
+        for i in range(0,len(s)-1,2):
+            ans+=s[i]
+            # print(s[i],end="")
+        tmp+=ans[::-1]
+        ans+=tmp
+    else:
+        for i in range(0,len(s)-1,2):
+            ans+=s[i]
+            # print(s[i],end="")
+        ans+=ans[::-1]
+print(ans)
+    
---
+++ Bonus Problem in String/LongestNon-PalindromicString.py
@@ -0,0 +1,7 @@
+s=input()
+if len(set(s))==1:
+    print(0)
+elif(s==s[::-1]):
+    print(len(s)-1)
+else:
+    print(len(s))
